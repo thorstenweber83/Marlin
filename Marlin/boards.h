530646772669c7d4c4028d5abb1ccafd677f0d01
Thorsten Weber <cpt.chaos83@googlemail.com>
2017-09-02 17:29:38 +0200

    add board configuration for flsun i3 printer like suggested here: https://www.thingiverse.com/groups/flsun-prusa-user-group/topic:15069#comment-1489816

--- Marlin/boards.h
+++ Marlin/boards.h
@@ -100,6 +100,7 @@
 #define BOARD_BAM_DICE_DUE      402  // 2PrintBeta BAM&DICE Due with STK drivers
 #define BOARD_BQ_ZUM_MEGA_3D    503  // bq ZUM Mega 3D
 #define BOARD_ZRIB_V20          504  // zrib V2.0 control board (Chinese knock off RAMPS replica)
+#define BOARD_BT7272A           431  // 3DYMY BT7272A RAMPS 1.4Plus with 2004LCD
 
 #define MB(board) (MOTHERBOARD==BOARD_##board)
 
