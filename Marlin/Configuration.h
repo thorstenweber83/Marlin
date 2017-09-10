351029dd0b559e0eb959d4923eccf04b9df3a787
Thorsten Weber <cpt.chaos83@googlemail.com>
2017-09-10 23:08:13 +0200

    update z probe z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -682,7 +682,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -18  // X offset: -left  +right  [of the nozzle] // no floats here!
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 11  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.5  // Z offset: -below +above  [the nozzle]      // here floats are possible
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.5  // Z offset: -below +above  [the nozzle]      // here floats are possible ... really???
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
