ff0526456c6794939b93f5a6e1345d9340e1da20
Thorsten Weber <cpt.chaos83@googlemail.com>
2018-03-30 23:54:24 +0200

    set acceleration and feedrate values for stealth chop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -562,7 +562,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 120, 120, 2, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -570,7 +570,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 1800, 1800, 50, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
