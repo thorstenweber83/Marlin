c7cec07fb664a0a9366b001912ca834ad7cb6e88
Thorsten Weber <cpt.chaos83@googlemail.com>
2018-04-11 00:41:00 +0200

    fix x-axis steps/mm for GT2 belt
    because we are using this upgrade:
    https://hackaday.io/project/14823-monoprice-select-mini-maximum-3d-printer-mods/log/60053-x-axis-gt2-belt-upgrade-e3dv6-carriage

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -555,7 +555,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 93, 93, 1097.5, 97 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 93, 1097.5, 97 }
 
 /**
  * Default Max Feed Rate (mm/s)
