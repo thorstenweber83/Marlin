659957887f758759b4057322d28e49a4ebd7d259
Thorsten Weber <cpt.chaos83@googlemail.com>
2018-05-31 19:09:46 +0200

    TMC2130: increase X axis current after losing steps on a longer print 600 -> 700 mA

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1060,7 +1060,7 @@
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          600  // rms current in mA. Multiply by 1.41 for peak current.
+  #define X_CURRENT          700  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
 
   #define Y_CURRENT          700
