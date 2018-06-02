31febd87f8faf311e7abdb51a8bdc68a8c17ee14
Thorsten Weber <cpt.chaos83@googlemail.com>
2018-06-02 20:22:32 +0200

    TMC2130: increase X + Y axis current after losing steps on a longer print 700 -> 800 mA

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -562,7 +562,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 120, 120, 2, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 120, 120, 4, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
