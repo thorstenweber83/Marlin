3b26470c142130b0b4c6d39261d9214290f2fc4f
Thorsten Weber <cpt.chaos83@googlemail.com>
2018-06-02 20:23:08 +0200

    increase Z axis feedrate this is possible thanks to the new NEMA 17 motor

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1841,7 +1841,7 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT     700  // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT     800  // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_MICROSTEPS   16  // 0..256
     #define X_RSENSE     0.11
     #define X_CHAIN_POS    -1  // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
@@ -1855,7 +1855,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT     700
+    #define Y_CURRENT     800
     #define Y_MICROSTEPS   16
     #define Y_RSENSE     0.11
     #define Y_CHAIN_POS    -1
