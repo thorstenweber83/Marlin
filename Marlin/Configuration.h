b4afdaea152c00fa4e424d444128c33367b26c4c
Thorsten Weber <cpt.chaos83@googlemail.com>
2018-06-06 23:01:44 +0200

    set up titan extruder clone (trianglelab)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -594,7 +594,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 145.25 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 416 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -828,7 +828,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR false
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
