8b167ecd63a6c4db647597888d6364d5021cf5ae
Thorsten Weber <cpt.chaos83@googlemail.com>
2020-04-03 18:57:46 +0200

    increase bed size to 180x195

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -859,11 +859,11 @@
 
 // The size of the print bed
 #define X_BED_SIZE 180
-#define Y_BED_SIZE 185
+#define Y_BED_SIZE 195
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -5
-#define Y_MIN_POS -5
+#define Y_MIN_POS 0
 #define Z_MIN_POS -100
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
